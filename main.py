e7d71deeb6f9e226ea1d48c546a45d245799a534
j-hnn <john.weng@searcyschools.org>
2024-05-16 10:45:00 -0500

    finished

--- main.py
+++ main.py
@@ -1,53 +1,39 @@
 import emoji
 from emoji import emojize
 
-attempts = 3
+lives = 10
 answers = ["titanic", "shrek", "toy story", "jurassic park", "godzilla", "jaws", "the martian", "transformers", "terminator", "john wick"]
-losing = False
+movies = [emojize(":ice:") + emojize(":motor_boat:") + emojize(":two_hearts:"),
+              emojize(":nauseated_face:") + emojize(":onion:") + emojize(":donkey:") + emojize(":princess:"),
+              emojize(":cowboy_hat_face:") + emojize(":alien:") + emojize(":open_book:"),
+              emojize(":sauropod:") + emojize(":dna:") + emojize(":national_park:"),
+              emojize(":fire:") + emojize(":lizard:")+ emojize(":ogre:"),
+              emojize(":shark:") + emojize(":speedboat:"),
+              emojize(":rocket:") + emojize(":ringed_planet:") + emojize(":potato:") + emojize(":astronaut:"),
+              emojize(":robot:") + emojize(":alien:") + emojize(":automobile:"),
+              emojize(":mechanical_arm:") + emojize(":mechanical_leg:") + emojize(":smiling_face_with_sunglasses:") + emojize(":robot:"),
+              emojize(":dog_face:") + emojize(":person_in_suit_levitating:") + emojize(":water_pistol:")]
 
-def guess(answer, attempts_left):
-    while True: #lives > 0
-        attempt = input("The movie is: ")
-        if attempt.lower() == answer:
-            print("Correct!\n")
-            break
-        else:
-            print("That was incorrect!")
-            attempts_left -= 1
-            print("Lives Left: " + str(attempts_left))
-            print("")
-            if attempts_left == 0:
-                 print("You lost!")
-                 losing = True
-                 return losing
 
-def round(round, emojis, is_losing):
-    print("Round " + round)
-    while not is_losing:
-        print(emojis)
-        guess(answers[int(round) - 1], attempts)
-        if attempts <= 0:
-                print("You lost!")
+print("Welcome to my emoji movie guessing game!")
+input("Press Enter to Begin!\n")
+
+if lives > 0:
+    for i in range(len(movies)):
+        print("Round " + str(i+1))
+        print(movies[i])
+        while lives > 0:
+            guess = input("The movie is: ")
+            if guess.lower() == answers[i]:
                 break
-        return attempts
-
-print("Welcome to the movie emoji guesser!")
-print("You will be given some emojis and guess what movie it is!")
-input("Press Enter to Begin\n")
-
-while True: 
-    round("1", emojize(":ice:") + emojize(":motor_boat:") + emojize(":two_hearts:"), losing)
-    round("2", emojize(":nauseated_face:") + emojize(":onion:") + emojize(":donkey:") + emojize(":princess:"), losing)
-    round("3", emojize(":cowboy_hat_face:") + emojize(":alien:") + emojize(":open_book:"), losing)
-    round("4", emojize(":sauropod:") + emojize(":dna:") + emojize(":national_park:"), losing)
-    round("5", emojize(":fire:") + emojize(":lizard:")+ emojize(":ogre:"), losing)
-    round("6", emojize(":shark:") + emojize(":speedboat:"), losing)
-    round("7", emojize(":rocket:") + emojize(":ringed_planet:") + emojize(":potato:") + emojize(":astronaut:"), losing)
-    round("8", emojize(":robot:") + emojize(":alien:") + emojize(":automobile:"), losing)
-    round("9", emojize(":mechanical_arm:") + emojize(":mechanical_leg:") + emojize(":smiling_face_with_sunglasses:") + emojize(":robot:"), losing)
-    round("10", emojize(":dog_face:") + emojize(":person_in_suit_levitating:") + emojize(":water_pistol:"), losing)
-
-    if losing:
-         print("You lost!")
-
-print("You won!")
+            else:
+                lives -= 1
+                print("Wrong movie!")
+                print(f"You have {lives} lives left.\n")
+                continue
+        if lives <= 0:
+            break
+if lives > 0:
+    print("You won!")
+else:
+    print("You lost!")
